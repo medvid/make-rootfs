2fbd9dc3556e8ba53538930e832f9f42728372d1
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-02 23:19:04 +0300

    meson: depends on setuptools

--- pkg/meson.mk
+++ pkg/meson.mk
@@ -2,6 +2,7 @@ pkg_ver  := 0.55.0
 pkg_repo := https://github.com/mesonbuild/meson
 pkg_site := $(pkg_repo)/releases/download/$(pkg_ver)
 pkg_copy := true
+pkg_deps := setuptools
 
 pkg_build := python3 $(pkg_srcdir)/setup.py build
 
