7735106ee1cacd020849c68f075c76abe2343641
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 22:39:20 +0300

    libudev-zero: add 0.3.1

---
+++ pkg/libudev-zero.mk
@@ -0,0 +1,10 @@
+pkg_ver  := 0.3.1
+pkg_repo := https://github.com/illiliti/libudev-zero
+pkg_url  := $(pkg_repo)/archive/$(pkg_ver).tar.gz
+pkg_copy := true
+
+pkg_build := make libudev.a libudev.pc
+
+pkg_install := install -m 644 libudev.a $(OUT_DIR)/usr/lib/libudev.a && \
+	install -m 644 udev.h $(OUT_DIR)/usr/include/libudev.h && \
+	install -m 644 libudev.pc $(OUT_DIR)/usr/lib/pkgconfig/libudev.pc
