174e21e4ef89e91838e4318b411333f5d87bbac1
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 21:04:42 +0300

    ncurses: add 6.2

---
+++ pkg/ncurses.mk
@@ -0,0 +1,28 @@
+pkg_ver  := 6.2
+pkg_site := https://invisible-mirror.net/archives/ncurses
+
+pkg_configure := $(pkg_srcdir)/configure \
+	--build=$(HOST) \
+	--host=$(TARGET) \
+	--prefix=/usr \
+	--sysconfdir=/etc \
+	--without-ada \
+	--without-cxx \
+	--without-cxx-binding \
+	--without-manpages \
+	--without-tack \
+	--without-tests \
+	--enable-pc-files \
+	--with-build-cflags="$(HOST_CFLAGS)" \
+	--with-build-ldflags="$(HOST_LDFLAGS)" \
+	--without-debug \
+	--without-dlsym \
+	--disable-lib-suffixes \
+	--disable-rpath-hack \
+	--enable-symlinks \
+	--with-pthread \
+	--without-develop
+
+pkg_build := make
+
+pkg_install := make install DESTDIR=$(OUT_DIR)
