d013c8ddaffed13e732c4538ea1077deac5ec9c8
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 22:37:27 +0300

    harfbuzz: improve pkg_url

--- pkg/harfbuzz.mk
+++ pkg/harfbuzz.mk
@@ -1,6 +1,6 @@
 pkg_ver  := 2.7.0
 pkg_repo := https://github.com/harfbuzz/harfbuzz
-pkg_url  := $(pkg_repo)/archive/2.7.0.tar.gz
+pkg_url  := $(pkg_repo)/archive/$(pkg_ver).tar.gz
 pkg_deps := freetype fontconfig cairo glib
 
 # TODO: check why Requires.private is not parsed from fontconfig.pc
