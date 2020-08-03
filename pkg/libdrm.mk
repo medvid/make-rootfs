0db39b244feca4e04d4a1ac0c593fad28a6e209b
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 22:57:03 +0300

    libdrm: add 2.4.102

---
+++ pkg/libdrm.mk
@@ -0,0 +1,35 @@
+pkg_ver  := 2.4.102
+pkg_repo := https://gitlab.freedesktop.org/mesa/drm
+pkg_site := https://dri.freedesktop.org/libdrm
+pkg_deps := libudev-zero
+
+# TODO: figure out how to parse from NM and patch properly
+pkg_prepare := find -L $(pkg_srcdir) -name meson.build \
+	-exec sed -e "s/'nm'/'llvm-nm'/g" -e "s/shared_library/library/g" -i {} \;
+
+# TODO: -Dintel=true requires libpciaccess
+pkg_configure := meson \
+	--prefix=/usr \
+	--sysconfdir=/etc \
+	-Ddefault_library=static \
+	-Dlibkms=true \
+	-Dintel=false \
+	-Dradeon=true \
+	-Damdgpu=true \
+	-Dnouveau=true \
+	-Dvmwgfx=true \
+	-Domap=false \
+	-Dexynos=false \
+	-Dfreedreno=false \
+	-Dtegra=false \
+	-Dvc4=false \
+	-Detnaviv=false \
+	-Dcairo-tests=false \
+	-Dman-pages=false \
+	-Dvalgrind=false \
+	-Dudev=true \
+	$(pkg_srcdir) $(pkg_objdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja install
