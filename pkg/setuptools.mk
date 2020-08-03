6aa51844f905a7a610f15fc889f78eddb82a6a08
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 07:09:49 +0300

    setuptools: fix pkg_install

--- pkg/setuptools.mk
+++ pkg/setuptools.mk
@@ -3,6 +3,8 @@ pkg_repo := https://github.com/pypa/setuptools
 pkg_site := https://files.pythonhosted.org/packages/source/s/setuptools
 pkg_copy := true
 
+pkg_configure := python3 bootstrap.py
+
 pkg_build := python3 setup.py build
 
-pkg_install := python3 setup.py install --optimize=1 --skip-build --root=$(OUT_DIR)
+pkg_install := python3 setup.py install --optimize=1 --skip-build --prefix=/usr --root=$(OUT_DIR)
