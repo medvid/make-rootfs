095008ad4dbd5eabeb158cf41d623b7b556a4dfe
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 22:22:27 +0300

    tmux: add 3.1b

---
+++ pkg/tmux.mk
@@ -0,0 +1,16 @@
+pkg_ver  := 3.1b
+pkg_repo := https://github.com/tmux/tmux
+pkg_site := $(pkg_repo)/releases/download/$(pkg_ver)
+pkg_deps := ncurses libevent
+
+pkg_configure := $(pkg_srcdir)/configure \
+	--build=$(HOST) \
+	--host=$(TARGET) \
+	--prefix=/usr \
+	--sysconfdir=/etc \
+	--disable-silent-rules \
+	--enable-static
+
+pkg_build := make
+
+pkg_install := make install DESTDIR=$(OUT_DIR)
