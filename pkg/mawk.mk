a401f0cbb0b83c8146c4333504b4f43f779ffed9
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-03 14:37:02 +0300

    mawk: update pkg_site

--- pkg/mawk.mk
+++ pkg/mawk.mk
@@ -1,6 +1,6 @@
 pkg_ver  := 1.3.4-20200120
 pkg_repo := https://github.com/ThomasDickey/mawk-snapshots
-pkg_site := ftp://ftp.invisible-island.net/mawk
+pkg_site := https://invisible-mirror.net/archives/mawk
 
 pkg_configure := $(pkg_srcdir)/configure \
 	--build=$(HOST) \
