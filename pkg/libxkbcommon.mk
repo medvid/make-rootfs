d5a66920298393b7eef46e1dd89941a857b7434a
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-08 15:50:44 +0300

    libxkbcommon: add 0.10.0

---
+++ pkg/libxkbcommon.mk
@@ -0,0 +1,18 @@
+pkg_ver  := 0.10.0
+pkg_repo := https://github.com/xkbcommon/libxkbcommon
+pkg_site := https://xkbcommon.org/download
+pkg_deps := wayland wayland-protocols xkeyboard-config
+
+pkg_configure := meson \
+	--prefix=/usr \
+	--sysconfdir=/etc \
+	-Ddefault_library=static \
+	-Dxkb-config-root=/usr/share/X11/xkb \
+	-Denable-x11=false \
+	-Denable-docs=false \
+	-Denable-xkbregistry=false \
+	$(pkg_srcdir) $(pkg_objdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja install
