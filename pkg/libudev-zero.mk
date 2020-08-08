13e76c451cb0dcd1b9a0c467db136a776f25ae0e
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-08 14:14:42 +0300

    libudev-zero: fix PREFIX for libudev.pc

--- pkg/libudev-zero.mk
+++ pkg/libudev-zero.mk
@@ -3,7 +3,7 @@ pkg_repo := https://github.com/illiliti/libudev-zero
 pkg_url  := $(pkg_repo)/archive/$(pkg_ver).tar.gz
 pkg_copy := true
 
-pkg_build := make libudev.a libudev.pc
+pkg_build := make libudev.a libudev.pc PREFIX=/usr
 
 pkg_install := install -m 644 libudev.a $(OUT_DIR)/usr/lib/libudev.a && \
 	install -m 644 udev.h $(OUT_DIR)/usr/include/libudev.h && \
