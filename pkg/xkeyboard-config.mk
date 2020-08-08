e077d5373a1f9f103ab37bfaa355cd583f281b11
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-08 15:49:44 +0300

    xkeyboard-config: add 2.30

---
+++ pkg/xkeyboard-config.mk
@@ -0,0 +1,16 @@
+pkg_ver  := 2.30
+pkg_repo := https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config
+pkg_site := http://www.x.org/releases/individual/data/xkeyboard-config
+
+pkg_configure := $(pkg_srcdir)/configure \
+	--build=$(HOST) \
+	--host=$(TARGET) \
+	--prefix=/usr \
+	--disable-silent-rules \
+	--disable-runtime-deps \
+	--disable-nls \
+	--without-xsltproc
+
+pkg_build := make
+
+pkg_install := make install DESTDIR=$(OUT_DIR)
