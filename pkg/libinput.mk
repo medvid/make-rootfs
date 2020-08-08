aecefe70147e37c3746efd672cba0c1cb4d4fe37
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-08 14:49:47 +0300

    libinput: add 1.16.0

---
+++ pkg/libinput.mk
@@ -0,0 +1,23 @@
+pkg_ver  := 1.16.0
+pkg_repo := https://gitlab.freedesktop.org/libinput/libinput
+pkg_site := https://www.freedesktop.org/software/libinput
+pkg_deps := libevdev mtdev
+
+pkg_prepare := sed -e "s/shared_library/library/g" -i $(pkg_srcdir)/meson.build
+
+pkg_configure := meson \
+	-Ddefault_library=static \
+	--prefix=/usr \
+	--sysconfdir=/etc \
+	--mandir=/usr/share/man \
+	--localstatedir=/var \
+	-Dlibwacom=false \
+	-Ddebug-gui=false \
+	-Dtests=false \
+	-Ddocumentation=false \
+	-Dzshcompletiondir=no \
+	$(pkg_srcdir) $(pkg_objdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja install
