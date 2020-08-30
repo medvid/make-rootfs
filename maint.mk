8c1f3afa28bbfb8056b27a7a5fbf6bf9d35181fe
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    maint.mk: fix config files destination

--- maint.mk
+++ maint.mk
@@ -2,7 +2,7 @@ update-all:
 	./pkg/maint/update-all.sh
 
 update-config:
-	curl -sSf https://git.savannah.gnu.org/cgit/automake.git/plain/lib/config.guess -o pkg/config.guess
-	curl -sSf https://git.savannah.gnu.org/cgit/automake.git/plain/lib/config.sub   -o pkg/config.sub
+	curl -sSf https://git.savannah.gnu.org/cgit/automake.git/plain/lib/config.guess -o pkg/files/config.guess
+	curl -sSf https://git.savannah.gnu.org/cgit/automake.git/plain/lib/config.sub   -o pkg/files/config.sub
 
 .PHONY: update-all update-config
