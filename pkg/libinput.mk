018b9c74b653244cd43998e1fdd1a2a425edeea3
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    libinput: support cross-compilation

--- pkg/libinput.mk
+++ pkg/libinput.mk
@@ -6,11 +6,12 @@ pkg_deps := libevdev mtdev
 pkg_prepare := sed -e "s/shared_library/library/g" -i $(pkg_srcdir)/meson.build
 
 pkg_configure := meson \
-	-Ddefault_library=static \
+	--cross-file $(TARGET).txt \
 	--prefix=/usr \
 	--sysconfdir=/etc \
 	--mandir=/usr/share/man \
 	--localstatedir=/var \
+	-Ddefault_library=static \
 	-Dlibwacom=false \
 	-Ddebug-gui=false \
 	-Dtests=false \
