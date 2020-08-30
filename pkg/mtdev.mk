ddd66ed305f75db10f846ce62dac85e1e6e09c1a
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    mtdev: fix cross-compilation

--- pkg/mtdev.mk
+++ pkg/mtdev.mk
@@ -1,7 +1,12 @@
 pkg_ver  := 1.1.6
 pkg_site := http://bitmath.org/code/mtdev
 
+pkg_prepare := cp -v $(ROOT_DIR)/$(PKG_DIR)/files/config.sub \
+	$(pkg_srcdir)/config-aux/config.sub
+
 pkg_configure := $(pkg_srcdir)/configure \
+	--build=$(HOST) \
+	--host=$(TARGET) \
 	--prefix=/usr \
 	--disable-silent-rules \
 	--disable-shared \
