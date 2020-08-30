1e4dd6510e87494467d1b53d7d4949b403ba1534
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    bc: run configure from pkg_objdir

--- pkg/bc.mk
+++ pkg/bc.mk
@@ -3,7 +3,7 @@ pkg_repo := https://git.yzena.com/gavin/bc
 pkg_site := https://github.com/gavinhoward/bc/releases/download/$(pkg_ver)
 pkg_copy := true
 
-pkg_configure := $(pkg_srcdir)/configure \
+pkg_configure := $(pkg_objdir)/configure \
 	--prefix=/usr \
 	--disable-nls \
 	--disable-man-pages
