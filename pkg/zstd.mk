af619685f37e7ee082e46a02be7b3fd886bdd365
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    zstd: use meson

--- pkg/zstd.mk
+++ pkg/zstd.mk
@@ -1,8 +1,17 @@
 pkg_ver  := 1.4.5
 pkg_repo := https://github.com/facebook/zstd
 pkg_site := $(pkg_repo)/releases/download/v$(pkg_ver)
-pkg_copy := true
 
-pkg_build := make -C lib libzstd.a libzstd.pc HAVE_PTHREAD=1 HAVE_ZLIB=0 HAVE_LZMA=0 HAVE_LZ4=0 PREFIX=/usr
+pkg_configure := meson \
+	--prefix=/usr \
+	--sysconfdir=/etc \
+	-Ddefault_library=static \
+	-Dbin_programs=false \
+	-Dzlib=disabled \
+	-Dlzma=disabled \
+	-Dlz4=disabled \
+	$(pkg_srcdir)/build/meson $(pkg_objdir)
 
-pkg_install := make -C lib install-pc install-static install-includes PREFIX=/usr DESTDIR=$(OUT_DIR)
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja install
