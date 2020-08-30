37e402965fd1aec5c80e166e51e3f87b383f3b5c
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    libinput: update to 1.16.1

--- pkg/libinput.mk
+++ pkg/libinput.mk
@@ -1,4 +1,4 @@
-pkg_ver  := 1.16.0
+pkg_ver  := 1.16.1
 pkg_repo := https://gitlab.freedesktop.org/libinput/libinput
 pkg_site := https://www.freedesktop.org/software/libinput
 pkg_deps := libevdev mtdev
