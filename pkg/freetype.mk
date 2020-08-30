f632feb78a5b68e4e959e6d77ef5196c9de0d3c0
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    freetype: FT_WITH_BZIP2=OFF

--- pkg/freetype.mk
+++ pkg/freetype.mk
@@ -1,7 +1,7 @@
 pkg_ver  := 2.10.2
 pkg_repo := https://git.savannah.gnu.org/git/freetype/freetype2.git
 pkg_site := https://download.savannah.gnu.org/releases/freetype
-pkg_deps := zlib bzip2 libpng
+pkg_deps := zlib libpng
 
 # TODO: figure out circular dependency on harfbuzz
 pkg_configure := cmake -G Ninja $(pkg_srcdir) \
@@ -12,10 +12,11 @@ pkg_configure := cmake -G Ninja $(pkg_srcdir) \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
 	-DBUILD_STATIC_LIBS:BOOL=ON \
 	-DFT_WITH_ZLIB:BOOL=ON \
-	-DFT_WITH_BZIP2:BOOL=ON \
+	-DFT_WITH_BZIP2:BOOL=OFF \
 	-DFT_WITH_PNG:BOOL=ON \
 	-DFT_WITH_HARFBUZZ:BOOL=OFF \
 	-DFT_WITH_BROTLI:BOOL=OFF \
+	-DCMAKE_DISABLE_FIND_PACKAGE_BZip2:BOOL=ON \
 	-DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz:BOOL=ON \
 	-DCMAKE_DISABLE_FIND_PACKAGE_BrotliDec:BOOL=ON
 
