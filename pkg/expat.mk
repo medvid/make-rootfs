d58da55f715251a7de2b3a9a3e496f994e0f962a
Volodymyr Medvid <vmedvid@riseup.net>
2020-08-30 10:43:02 +0300

    expat: use cmake for build

--- pkg/expat.mk
+++ pkg/expat.mk
@@ -2,18 +2,16 @@ pkg_ver  := 2.2.9
 pkg_repo := https://github.com/libexpat/libexpat
 pkg_site := $(pkg_repo)/releases/download/R_$(subst .,_,$(pkg_ver))
 
-pkg_configure := $(pkg_srcdir)/configure \
-	--build=$(HOST) \
-	--host=$(TARGET) \
-	--prefix=/usr \
-	--disable-silent-rules \
-	--disable-shared \
-	--without-xmlwf \
-	--without-examples \
-	--without-tests \
-	--with-sys-getrandom \
-	--without-docbook
+pkg_configure := cmake -G Ninja $(pkg_srcdir) \
+	-DCMAKE_BUILD_TYPE:STRING=Release \
+	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
+	-DCMAKE_INSTALL_LIBDIR:STRING=lib \
+	-DCMAKE_SYSROOT=$(OUT_DIR) \
+	-DEXPAT_BUILD_EXAMPLES:BOOL=OFF \
+	-DEXPAT_BUILD_TESTS:BOOL=OFF \
+	-DEXPAT_SHARED_LIBS:BOOL=OFF \
+	-DEXPAT_BUILD_DOCS:BOOL=OFF
 
-pkg_build := make
+pkg_build := ninja -v
 
-pkg_install := make install DESTDIR=$(OUT_DIR)
+pkg_install := DESTDIR=$(OUT_DIR) ninja -v install
