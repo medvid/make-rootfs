1eb9ae4cbe33af895b8da858939ef0a70103fc11
Volodymyr Medvid <vmedvid@riseup.net>
2020-12-28 20:59:46 +0200

    cmake: use cmake_pkg_configure

--- pkg/cmake.mk
+++ pkg/cmake.mk
@@ -3,9 +3,7 @@ pkg_repo := https://github.com/Kitware/CMake
 pkg_site := $(pkg_repo)/releases/download/v$(pkg_ver)
 pkg_deps := openssl
 
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=/usr \
+pkg_configure := $(cmake_pkg_configure) \
 	-DCMAKE_DATA_DIR=share/cmake \
 	-DCMAKE_DOC_DIR=share/doc/cmake \
 	-DCMAKE_C_COMPILER=clang \
@@ -16,7 +14,8 @@ pkg_configure := cmake -G Ninja $(pkg_srcdir) \
 	-DCMAKE_USE_SYSTEM_LIBRARIES:BOOL=OFF \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DBUILD_CursesDialog:BOOL=OFF \
-	-DBUILD_QtDialog:BOOL=OFF
+	-DBUILD_QtDialog:BOOL=OFF \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
