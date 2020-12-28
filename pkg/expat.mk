93072c2aebd565ddaff1f7ced613e9e0cd0f68be
Volodymyr Medvid <vmedvid@riseup.net>
2020-12-28 21:01:52 +0200

    expat: use cmake_pkg_configure

--- pkg/expat.mk
+++ pkg/expat.mk
@@ -2,15 +2,12 @@ pkg_ver  := 2.2.10
 pkg_repo := https://github.com/libexpat/libexpat
 pkg_site := $(pkg_repo)/releases/download/R_$(subst .,_,$(pkg_ver))
 
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
-	-DCMAKE_INSTALL_LIBDIR:STRING=lib \
-	-DCMAKE_SYSROOT=$(OUT_DIR) \
+pkg_configure := $(cmake_pkg_configure) \
 	-DEXPAT_BUILD_EXAMPLES:BOOL=OFF \
 	-DEXPAT_BUILD_TESTS:BOOL=OFF \
 	-DEXPAT_SHARED_LIBS:BOOL=OFF \
-	-DEXPAT_BUILD_DOCS:BOOL=OFF
+	-DEXPAT_BUILD_DOCS:BOOL=OFF \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
