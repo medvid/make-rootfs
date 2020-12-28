aff04132fdc86be2f6b550b9e356f4793f88da14
Volodymyr Medvid <vmedvid@riseup.net>
2020-12-28 21:00:49 +0200

    freetype: use cmake_pkg_configure

--- pkg/freetype.mk
+++ pkg/freetype.mk
@@ -3,12 +3,7 @@ pkg_repo := https://git.savannah.gnu.org/git/freetype/freetype2.git
 pkg_site := https://download.savannah.gnu.org/releases/freetype
 pkg_deps := zlib libpng
 
-# TODO: figure out circular dependency on harfbuzz
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
-	-DCMAKE_INSTALL_LIBDIR:STRING=lib \
-	-DCMAKE_SYSROOT=$(OUT_DIR) \
+pkg_configure := $(cmake_pkg_configure) \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
 	-DFT_WITH_ZLIB:BOOL=ON \
 	-DFT_WITH_BZIP2:BOOL=OFF \
@@ -17,7 +12,8 @@ pkg_configure := cmake -G Ninja $(pkg_srcdir) \
 	-DFT_WITH_BROTLI:BOOL=OFF \
 	-DCMAKE_DISABLE_FIND_PACKAGE_BZip2:BOOL=ON \
 	-DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz:BOOL=ON \
-	-DCMAKE_DISABLE_FIND_PACKAGE_BrotliDec:BOOL=ON
+	-DCMAKE_DISABLE_FIND_PACKAGE_BrotliDec:BOOL=ON \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
