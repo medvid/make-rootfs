ea846a17b83565a433e1a334a4ee7ec669fdfb3a
Volodymyr Medvid <vmedvid@riseup.net>
2021-01-10 11:47:19 +0200

    pcre: switch to CMake

--- pkg/pcre.mk
+++ pkg/pcre.mk
@@ -1,18 +1,25 @@
 pkg_ver  := 8.44
 pkg_site := https://ftp.pcre.org/pub/pcre
 
-pkg_configure := $(pkg_srcdir)/configure \
-	--build=$(HOST) \
-	--host=$(TARGET) \
-	--prefix=/usr \
-	--disable-silent-rules \
-	--disable-shared \
-	--disable-cpp \
-	--enable-jit \
-	--enable-utf8 \
-	--without-pic
+pkg_configure := $(cmake_pkg_configure) \
+	-DPCRE_BUILD_PCRE8:BOOL=ON \
+	-DPCRE_BUILD_PCRE16:BOOL=OFF \
+	-DPCRE_BUILD_PCRE32:BOOL=OFF \
+	-DPCRE_BUILD_PCRECPP:BOOL=OFF \
+	-DPCRE_SUPPORT_JIT:BOOL=ON \
+	-DPCRE_SUPPORT_UTF:BOOL=ON \
+	-DPCRE_BUILD_PCREGREP:BOOL=OFF \
+	-DPCRE_BUILD_TESTS:BOOL=OFF \
+	-DPCRE_SUPPORT_LIBBZ2:BOOL=OFF \
+	-DPCRE_SUPPORT_LIBZ:BOOL=OFF \
+	-DPCRE_SUPPORT_LIBEDIT:BOOL=OFF \
+	-DPCRE_SUPPORT_LIBREADLINE:BOOL=OFF \
+	-DCMAKE_DISABLE_FIND_PACKAGE_BZip2:BOOL=ON \
+	-DCMAKE_DISABLE_FIND_PACKAGE_ZLIB:BOOL=ON \
+	-DCMAKE_DISABLE_FIND_PACKAGE_Readline:BOOL=ON \
+	-DCMAKE_DISABLE_FIND_PACKAGE_Editline:BOOL=ON \
+	$(pkg_srcdir)
 
-pkg_build := make
+pkg_build := ninja -v
 
-# Do not install the executables
-pkg_install := make install-libLTLIBRARIES install-data DESTDIR=$(OUT_DIR)
+pkg_install := DESTDIR=$(OUT_DIR) ninja -v install
