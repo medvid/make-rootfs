238a53350ef23f4c4a3595ce32eb603edec46fdc
Volodymyr Medvid <vmedvid@riseup.net>
2021-01-10 23:21:25 +0200

    cmake: use system libraries

--- pkg/cmake.mk
+++ pkg/cmake.mk
@@ -1,21 +1,28 @@
 pkg_ver  := 3.19.2
 pkg_repo := https://github.com/Kitware/CMake
 pkg_site := $(pkg_repo)/releases/download/v$(pkg_ver)
-pkg_deps := openssl
+pkg_deps := curl expat zlib bzip2 zstd xz nghttp2 jsoncpp rhash libuv openssl
 
-pkg_configure := $(cmake_pkg_configure) \
+pkg_configure := LDFLAGS="$(LDFLAGS) -lcurl -lcrypto -lssl" \
+	$(cmake_pkg_configure) \
 	-DCMAKE_DATA_DIR=share/cmake \
 	-DCMAKE_DOC_DIR=share/doc/cmake \
-	-DCMAKE_C_COMPILER=clang \
-	-DCMAKE_CXX_COMPILER=clang++ \
-	-DCMAKE_EXE_LINKER_FLAGS="-static" \
-	-DCMAKE_LINKER=lld \
-	-DCMAKE_USE_OPENSSL:BOOL=ON \
-	-DCMAKE_USE_SYSTEM_LIBRARIES:BOOL=OFF \
+	-DCMAKE_USE_SYSTEM_LIBARCHIVE:BOOL=OFF \
+	-DCMAKE_USE_SYSTEM_CURL:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_EXPAT:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_ZLIB:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_BZIP2:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_ZSTD:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_LIBLZMA:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_NGHTTP2:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_JSONCPP:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_LIBRHASH:BOOL=ON \
+	-DCMAKE_USE_SYSTEM_LIBUV:BOOL=ON \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DBUILD_CursesDialog:BOOL=OFF \
 	-DBUILD_QtDialog:BOOL=OFF \
-	-DHAVE_POLL_FINE_EXITCODE=0 \
+	-DCMake_RUN_CXX_FILESYSTEM:INTERNAL=0 \
+	-DCMake_RUN_CXX_FILESYSTEM__TRYRUN_OUTPUT:INTERNAL="" \
 	$(pkg_srcdir)
 
 pkg_build := ninja -v
