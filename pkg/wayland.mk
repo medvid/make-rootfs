db3e4f6b24dc97e391347d4a233071542465f6ad
Volodymyr Medvid <vmedvid@riseup.net>
2021-01-11 22:02:41 +0200

    wayland: fix cross-compilation

--- pkg/wayland.mk
+++ pkg/wayland.mk
@@ -7,7 +7,9 @@ pkg_deps := libffi expat
 pkg_prepare := sed -e "s/'nm'/'llvm-nm'/g" -i $(pkg_srcdir)/egl/meson.build && \
 	sed -e "/'tests'/s/^\t/\t\#/" -i $(pkg_srcdir)/meson.build
 
-pkg_configure := $(meson_pkg_configure) \
+# workaround for wayland-scanner.pc native dependency
+pkg_configure := PKG_CONFIG_LIBDIR=$(OUT_DIR)/usr/lib/pkgconfig:$(ROOT_DIR)/usr/lib/pkgconfig \
+	PKG_CONFIG_SYSROOT_DIR= $(meson_pkg_configure) \
 	-Ddocumentation=false \
 	-Ddtd_validation=false \
 	$(pkg_srcdir) $(pkg_objdir)
