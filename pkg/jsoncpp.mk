490f4cd5b9587e5e44109370dc18e44b4e94a6e1
Volodymyr Medvid <vmedvid@riseup.net>
2021-01-16 23:25:19 +0200

    jsoncpp: disable tests

--- pkg/jsoncpp.mk
+++ pkg/jsoncpp.mk
@@ -8,7 +8,7 @@ pkg_repo := https://github.com/open-source-parsers/jsoncpp
 pkg_site := $(pkg_repo)/archive/$(pkg_ver)
 
 pkg_configure := $(cmake_pkg_configure) \
-	-DJSONCPP_WITH_TESTS:BOOL=ON \
+	-DJSONCPP_WITH_TESTS:BOOL=OFF \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
 	-DBUILD_STATIC_LIBS:BOOL=ON \
 	-DBUILD_OBJECT_LIBS:BOOL=OFF \
