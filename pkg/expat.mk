20726a38781004bf09f7850fd17311f6119a7dd7
Volodymyr Medvid <vmedvid@riseup.net>
2021-01-16 19:00:15 +0200

    expat: disable tests

--- pkg/expat.mk
+++ pkg/expat.mk
@@ -11,7 +11,7 @@ pkg_site := $(pkg_repo)/releases/download/R_$(subst .,_,$(pkg_ver))
 
 pkg_configure := $(cmake_pkg_configure) \
 	-DEXPAT_BUILD_EXAMPLES:BOOL=OFF \
-	-DEXPAT_BUILD_TESTS:BOOL=ON \
+	-DEXPAT_BUILD_TESTS:BOOL=OFF \
 	-DEXPAT_SHARED_LIBS:BOOL=OFF \
 	-DEXPAT_BUILD_DOCS:BOOL=OFF \
 	$(pkg_srcdir)
