59e49edfcee8b462a15ed0966c0b0e67d524ae62
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-01 09:16:26 +0200

    bc: update to 3.2.6

--- pkg/bc.mk
+++ pkg/bc.mk
@@ -3,7 +3,7 @@
 # https://github.com/void-linux/void-packages/blob/master/srcpkgs/bc-gh/template
 # https://code.foxkit.us/adelie/packages/-/blob/master/system/bc/APKBUILD
 
-pkg_ver  := 3.2.4
+pkg_ver  := 3.2.6
 pkg_repo := https://git.yzena.com/gavin/bc
 pkg_site := https://github.com/gavinhoward/bc/releases/download/$(pkg_ver)
 pkg_copy := true
