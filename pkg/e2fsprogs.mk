1f8d6a6d5451ba97f245e60fe20407bfcfff03ee
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-13 23:42:17 +0200

    e2fsprogs: fix pkg_install, add musl patch

--- pkg/e2fsprogs.mk
+++ pkg/e2fsprogs.mk
@@ -8,27 +8,29 @@
 pkg_ver  := 1.46.0
 pkg_repo := https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git
 pkg_site := https://www.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v$(pkg_ver)
+pkg_deps := libuuid
 
-# Note: AC_PROG_MKDIR_P doesn't like relative paths
-pkg_configure := $(pkg_objdir)/$(pkg_srcdir)/configure \
+pkg_configure := $(pkg_srcdir)/configure \
 	--build=$(HOST) \
 	--host=$(TARGET) \
 	--prefix=/usr \
 	--sbindir=/usr/bin \
 	--sysconfdir=/etc \
+	--disable-libuidd \
 	--disable-debugfs \
 	--disable-imager \
 	--enable-fsck \
 	--disable-uuidd \
-	--disable-nls \
-	--disable-fuse2fs
+	--disable-nls
 
-pkg_build := make
+pkg_build := make static-progs V=1
 
-pkg_check := make check
-
-pkg_install := install -m 755 e2fsck/e2fsck misc/mke2fs \
-	misc/tune2fs resize/resize2fs $(OUT_DIR)/usr/bin/ && \
+pkg_install := install -m 755 e2fsck/e2fsck.static $(OUT_DIR)/usr/bin/e2fsck && \
+	install -m 755 misc/blkid.static $(OUT_DIR)/usr/bin/blkid && \
+	install -m 755 misc/dumpe2fs.static $(OUT_DIR)/usr/bin/dumpe2fs && \
+	install -m 755 misc/mke2fs.static $(OUT_DIR)/usr/bin/mke2fs && \
+	install -m 755 misc/tune2fs.static $(OUT_DIR)/usr/bin/tune2fs && \
+	install -m 755 resize/resize2fs.static $(OUT_DIR)/usr/bin/resize2fs && \
 	install -m644 misc/mke2fs.conf $(OUT_DIR)/etc/ && \
 	ln -sf e2fsck $(OUT_DIR)/usr/bin/fsck.ext4 && \
-	ln -sf mke2fs $(OUT_DIR)/usr/bin/mkfs.ext4
+ 	ln -sf mke2fs $(OUT_DIR)/usr/bin/mkfs.ext4
