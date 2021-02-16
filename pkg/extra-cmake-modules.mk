530f83ef9d43b8a3f218a3b8f0a1d592c521ba88
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-16 19:26:17 +0200

    extra-cmake-modules: update to 5.79.0

--- pkg/extra-cmake-modules.mk
+++ pkg/extra-cmake-modules.mk
@@ -1,16 +1,15 @@
-pkg_ver  := 5.77.0
+pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/extra-cmake-modules
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
 
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
+pkg_configure := $(kde_pkg_configure) \
 	-DBUILD_TESTING:BOOL=FALSE \
 	-DCMAKE_DISABLE_FIND_PACKAGE_Sphinx:BOOL=ON \
 	-DCMAKE_DISABLE_FIND_PACKAGE_QCollectionGenerator:BOOL=ON \
 	-DBUILD_HTML_DOCS:BOOL=OFF \
 	-DBUILD_MAN_DOCS:BOOL=OFF \
-	-DBUILD_QTHELP_DOCS:BOOL=OFF
+	-DBUILD_QTHELP_DOCS:BOOL=OFF \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
