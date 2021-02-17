33fc3cfc994a230c2f84a215d309c9f86ce2728a
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-17 10:18:46 +0200

    kconfig: add 5.79.0

---
+++ pkg/kconfig.mk
@@ -0,0 +1,16 @@
+pkg_ver  := 5.79.0
+pkg_repo := https://invent.kde.org/frameworks/kconfig
+pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
+pkg_deps := extra-cmake-modules qt5
+
+pkg_configure := $(kde_pkg_configure) \
+	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DKCONFIG_USE_GUI:BOOL=ON \
+	-DKCONFIG_USE_DBUS:BOOL=OFF \
+	-DBUILD_QCH:BOOL=OFF \
+	-DBUILD_TESTING:BOOL=OFF \
+	$(pkg_srcdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja -v install
