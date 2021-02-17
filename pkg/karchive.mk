0d31acc71185a399730ac9cabf8477b2148b8458
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-17 10:16:49 +0200

    karchive: update to 5.79.0

--- pkg/karchive.mk
+++ pkg/karchive.mk
@@ -1,15 +1,12 @@
-pkg_ver  := 5.77.0
+pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/karchive
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
-pkg_deps := extra-cmake-modules qt zlib bzip2 xz
+pkg_deps := extra-cmake-modules qt5 zlib bzip2 xz
 
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
-	-DCMAKE_INSTALL_LIBDIR:STRING=lib \
-	-DCMAKE_SYSROOT=$(OUT_DIR) \
+pkg_configure := $(kde_pkg_configure) \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
-	-DBUILD_QCH:BOOL=OFF
+	-DBUILD_QCH:BOOL=OFF \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
