a483f98b4845118971b020963dcf102a35576167
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-17 10:18:27 +0200

    kcodecs: add 5.79.0

---
+++ pkg/kcodecs.mk
@@ -0,0 +1,14 @@
+pkg_ver  := 5.79.0
+pkg_repo := https://invent.kde.org/frameworks/kcodecs
+pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
+pkg_deps := extra-cmake-modules qt5
+
+pkg_configure := $(kde_pkg_configure) \
+	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DBUILD_QCH:BOOL=OFF \
+	-DBUILD_TESTING:BOOL=OFF \
+	$(pkg_srcdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja -v install
