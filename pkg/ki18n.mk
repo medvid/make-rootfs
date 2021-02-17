9a135817fb29b7b155c4e2fcd13e63c0e951aa58
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-17 10:17:17 +0200

    ki18n: update to 5.79.0

--- pkg/ki18n.mk
+++ pkg/ki18n.mk
@@ -1,17 +1,15 @@
-pkg_ver  := 5.77.0
+pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/ki18n
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
-pkg_deps := extra-cmake-modules qt
+pkg_deps := extra-cmake-modules qt5 gettext-tiny
 
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
-	-DCMAKE_INSTALL_LIBDIR:STRING=lib \
-	-DCMAKE_SYSROOT=$(OUT_DIR) \
+pkg_configure := $(kde_pkg_configure) \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DBUILD_WITH_QML:BOOL=OFF \
 	-DBUILD_QCH:BOOL=OFF \
 	-DENABLE_INOTIFY:BOOL=OFF \
-	-DENABLE_PROCSTAT:BOOL=OFF
+	-DENABLE_PROCSTAT:BOOL=OFF \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
