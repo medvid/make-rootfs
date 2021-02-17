e48a6d67a60c725b95e081a17f43fcec0c275bc4
Volodymyr Medvid <vmedvid@riseup.net>
2021-02-17 10:15:50 +0200

    kcoreaddons: update to 5.79.0

--- pkg/kcoreaddons.mk
+++ pkg/kcoreaddons.mk
@@ -1,18 +1,16 @@
-pkg_ver  := 5.77.0
+pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/kcoreaddons
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
-pkg_deps := extra-cmake-modules qt
+pkg_deps := extra-cmake-modules qt5
 
-pkg_configure := cmake -G Ninja $(pkg_srcdir) \
-	-DCMAKE_BUILD_TYPE:STRING=Release \
-	-DCMAKE_INSTALL_PREFIX:PATH=/usr \
-	-DCMAKE_INSTALL_LIBDIR:STRING=lib \
-	-DCMAKE_SYSROOT=$(OUT_DIR) \
+pkg_configure := $(kde_pkg_configure) \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
 	-DBUILD_QCH:BOOL=OFF \
+	-DBUILD_TESTING:BOOL=OFF \
 	-DENABLE_INOTIFY:BOOL=OFF \
 	-DENABLE_PROCSTAT:BOOL=OFF \
-	-DCMAKE_DISABLE_FIND_PACKAGE_FAM:BOOL=ON
+	-DCMAKE_DISABLE_FIND_PACKAGE_FAM:BOOL=ON \
+	$(pkg_srcdir)
 
 pkg_build := ninja -v
 
