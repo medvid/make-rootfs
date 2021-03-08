6eb18474350cb9f4bf3c632d20479c6c173d1f32
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 15:25:01 +0200

    libdrm: remove llvm-nm workaround

--- pkg/libdrm.mk
+++ pkg/libdrm.mk
@@ -10,10 +10,6 @@ pkg_repo := https://gitlab.freedesktop.org/mesa/drm
 pkg_site := https://dri.freedesktop.org/libdrm
 pkg_deps := libudev-zero
 
-# TODO: figure out how to parse from NM and patch properly
-pkg_prepare := find -L $(pkg_srcdir) -name meson.build \
-	-exec sed -e "s/'nm'/'llvm-nm'/g" -e "s/shared_library/library/g" -i {} \;
-
 # TODO: -Dintel=true requires libpciaccess
 pkg_configure := $(meson_pkg_configure) \
 	-Dlibkms=true \
