3d4c2daf19e9788f688810194af60abc287fc18d
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 15:09:12 +0200

    kservice: add 5.79.0

---
+++ pkg/kservice.mk
@@ -0,0 +1,20 @@
+# https://repology.org/project/kservice
+# https://git.alpinelinux.org/aports/tree/community/kservice/APKBUILD
+# https://github.com/void-linux/void-packages/blob/master/srcpkgs/kservice/template
+# https://code.foxkit.us/adelie/packages/blob/master/user/kservice/APKBUILD
+
+pkg_ver  := 5.79.0
+pkg_repo := https://invent.kde.org/frameworks/kservice
+pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
+pkg_deps := extra-cmake-modules qt5 kconfig kcoreaddons kcrash kdbusaddons ki18n
+
+pkg_configure := $(kde_pkg_configure) \
+	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DBUILD_QCH:BOOL=OFF \
+	-DBUILD_TESTING:BOOL=OFF \
+	-DCMAKE_DISABLE_FIND_PACKAGE_KF5DocTools:BOOL=ON \
+	$(pkg_srcdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja -v install
