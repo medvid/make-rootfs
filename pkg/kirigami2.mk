2c1f73089f09f033797ddb519e65e140680da019
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 15:20:54 +0200

    kirigami2: add 5.79.0

---
+++ pkg/kirigami2.mk
@@ -0,0 +1,19 @@
+# https://repology.org/project/kirigami2
+# https://git.alpinelinux.org/aports/tree/community/kirigami2/APKBUILD
+# https://github.com/void-linux/void-packages/blob/master/srcpkgs/kirigami2/template
+# https://code.foxkit.us/adelie/packages/blob/master/user/kirigami2/APKBUILD
+
+pkg_ver  := 5.79.0
+pkg_repo := https://invent.kde.org/frameworks/kirigami
+pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
+pkg_deps := extra-cmake-modules qt5 dbus kcoreaddons kconfig kwindowsystem
+
+pkg_configure := $(kde_pkg_configure) \
+	-DBUILD_SHARED_LIBS:BOOL=OFF \
+	-DBUILD_QCH:BOOL=OFF \
+	-DBUILD_TESTING:BOOL=OFF \
+	$(pkg_srcdir)
+
+pkg_build := ninja -v
+
+pkg_install := DESTDIR=$(OUT_DIR) ninja -v install
