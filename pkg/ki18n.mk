eda0cf890a9c2da2a749b049b75e223f3d098330
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 14:56:37 +0200

    ki18n: add packaging links

--- pkg/ki18n.mk
+++ pkg/ki18n.mk
@@ -1,3 +1,8 @@
+# https://repology.org/project/ki18n
+# https://git.alpinelinux.org/aports/tree/community/ki18n/APKBUILD
+# https://github.com/void-linux/void-packages/blob/master/srcpkgs/ki18n/template
+# https://code.foxkit.us/adelie/packages/blob/master/user/ki18n/APKBUILD
+
 pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/ki18n
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
