afc1f5e054ae58f4ce0f98e9a8c41d3a2157810a
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 14:50:08 +0200

    kauth: build with KAUTH_HELPER_BACKEND_NAME=FAKE

--- pkg/kauth.mk
+++ pkg/kauth.mk
@@ -1,3 +1,8 @@
+# https://repology.org/project/kauth
+# https://git.alpinelinux.org/aports/tree/community/kauth/APKBUILD
+# https://github.com/void-linux/void-packages/blob/master/srcpkgs/kauth/template
+# https://code.foxkit.us/adelie/packages/blob/master/user/kauth/APKBUILD
+
 pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/kauth
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
@@ -8,6 +13,7 @@ pkg_configure := $(kde_pkg_configure) \
 	-DBUILD_QCH:BOOL=OFF \
 	-DBUILD_TESTING:BOOL=OFF \
 	-DKAUTH_BACKEND_NAME:STRING="FAKE" \
+	-DKAUTH_HELPER_BACKEND_NAME:STRING="FAKE" \
 	$(pkg_srcdir)
 
 pkg_build := ninja -v
