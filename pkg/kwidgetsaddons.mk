841e2f77abd6fbc56e625aa39dcf80a2d29b11f9
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 14:48:02 +0200

    kwidgetsaddons: add packaging links

--- pkg/kwidgetsaddons.mk
+++ pkg/kwidgetsaddons.mk
@@ -1,3 +1,8 @@
+# https://repology.org/project/kwidgetsaddons
+# https://git.alpinelinux.org/aports/tree/community/kwidgetsaddons/APKBUILD
+# https://github.com/void-linux/void-packages/blob/master/srcpkgs/kwidgetsaddons/template
+# https://code.foxkit.us/adelie/packages/blob/master/user/kwidgetsaddons/APKBUILD
+
 pkg_ver  := 5.79.0
 pkg_repo := https://invent.kde.org/frameworks/kwidgetsaddons
 pkg_site := https://download.kde.org/stable/frameworks/$(basename $(pkg_ver))
