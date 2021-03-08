813ddd2c7d2cd6a8913c346f2a26657884638bc2
Volodymyr Medvid <vmedvid@riseup.net>
2021-03-08 23:57:56 +0200

    xkeyboard-config: update to 2.32

--- pkg/xkeyboard-config.mk
+++ pkg/xkeyboard-config.mk
@@ -3,7 +3,7 @@
 # https://github.com/distr1/distri/blob/master/pkgs/xkeyboard-config/build.textproto
 # https://github.com/void-linux/void-packages/blob/master/srcpkgs/xkeyboard-config/template
 
-pkg_ver  := 2.31
+pkg_ver  := 2.32
 pkg_repo := https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config
 pkg_site := http://www.x.org/releases/individual/data/xkeyboard-config
 
